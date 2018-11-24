58338d32336d0da7e97bc8e5c6c7706d23b2ac7b
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2018-11-24 00:05:49 -0500

    Dropdown menu fix
    -edit link for the menu button to be ignored using javascript: void(0)

--- dropdown.js
+++ dropdown.js
@@ -2,21 +2,22 @@
 toggle between hiding and showing the dropdown content */
 function dropdownClicked() {
     document.getElementById("dropdownInfo").classList.toggle("show");
-}
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.menu-button')) {
 
-    //list of all of the dropdown options for the button 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      //removes the drop down content if it is shown
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+      if (!event.target.matches('.menu-button')) {
+
+        //list of all of the dropdown options for the button
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          //removes the drop down content if it is shown
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
       }
     }
-  }
 }
