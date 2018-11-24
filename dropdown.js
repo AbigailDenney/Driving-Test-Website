9b65c068687d71f082ad0c4f2283a1609d380401
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2018-11-23 23:29:59 -0500

    Fixing dropdown by link
    Edit on the drop down list for the info
    
    -first testing on infor link before going to other links

--- dropdown.js
+++ dropdown.js
@@ -1,4 +1,4 @@
-/* When the user clicks on the button, 
+/* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function dropdownClicked() {
     document.getElementById("dropdownInfo").classList.toggle("show");
@@ -6,12 +6,14 @@ function dropdownClicked() {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.menu-button')) {
 
+    //list of all of the dropdown options for the button 
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
+      //removes the drop down content if it is shown
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
