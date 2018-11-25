8637cdabe57a67bd0e11f0bfeea7b6004e28ce19
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2018-11-25 15:25:13 -0500

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -8,7 +8,7 @@ function dropdownClicked(menuOption) {
     /*Close the dropdown menu if the user clicks outside of it or they selected
     a different menu button*/
     window.onclick = function(event) {
-      if (event.target.className != "menu-button" ||
+      if (!event.target.matches('.menu-button') ||
       event.target.id != previousButton && previousButton != undefined) {
 
         //list of all of the dropdown options for the button
@@ -22,12 +22,12 @@ function dropdownClicked(menuOption) {
           }
         }
       }
-
-      //displays the content
-      document.getElementById(menuOption).classList.toggle("show");
-
-      /*stores the current triggered id as the previous button clicked for the
-      next time the user clicks on a menu button*/
-      previousButton = event.target.id;
     }
+
+    //displays the content
+    document.getElementById(menuOption).classList.toggle("show");
+
+    /*stores the current triggered id as the previous button clicked for the
+    next time the user clicks on a menu button*/
+    previousButton = event.target.id;
 }
