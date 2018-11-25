4e5e40c3ece7c6ce72757c3fefe0e8198cd976f5
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2018-11-25 15:59:50 -0500

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -8,26 +8,34 @@ function dropdownClicked(menuOption) {
     /*Close the dropdown menu if the user clicks outside of it or they selected
     a different menu button*/
     window.onclick = function(event) {
-      if (!event.target.matches('.menu-button') ||
-      event.target.id != previousButton && previousButton != undefined) {
-
-        //list of all of the dropdown options for the button
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          //removes the drop down content if it is shown
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
-        }
+      if (!event.target.matches('.menu-button')) {
+        collapse();
+      } else if (event.target.id != previousButton && previousButton != undefined) {
+        collapse();
+        //displays the content
+        document.getElementById(menuOption).classList.toggle("show");
       }
 
       /*stores the current triggered id as the previous button clicked for the
       next time the user clicks on a menu button*/
       previousButton = event.target.id;
-
-      //displays the content
-      document.getElementById(menuOption).classList.toggle("show");
     }
+
+    //displays the content
+    document.getElementById(menuOption).classList.toggle("show");
+}
+
+
+
+function collapse() {
+  //list of all of the dropdown options for the button
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    //removes the drop down content if it is shown
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
 }
